1e9c36078c8f2c0258aac2470109eb87ba73c51b
Karimo64 <42246635+Karimo64@users.noreply.github.com>
2024-01-31 03:03:37 -0600

    Spaceship Update
    -Added new folders
       +Sprite folder with pngs
       +SFX folder with sounds
       +Font folder with font style
    -Implemented new file laser.py
    -Added move and shot functionality to spaceship
    -Modified main.py
    
    -Need to fix:
    Attribute read in line 43 of spaceship.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame, sys
+from spaceship import Spacecship #Call spaceship file
+#from laser import Laser
 
 pygame.init()
 
@@ -12,14 +14,31 @@ pygame.display.set_caption("Python Space Invaders")
 
 clock = pygame.time.Clock() #Needed for framerate
 
+spaceship = Spacecship(SCREEN_WIDTH, SCREEN_HEIGHT) #Initialize spaceship in the position given width and height
+spaceship_group = pygame.sprite.GroupSingle() #Start a group
+spaceship_group.add(spaceship)
+
+
+#Used to check the behavior of the laser movement
+#laser = Laser((100,100), 6, SCREEN_HEIGHT)
+#lasers_group = pygame.sprite.Group()
+#lasers_group.add(laser)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         
-        #Drawing a color in the screen
-        screen.fill(GREY)
+    #Update
+    spaceship_group.update()
+    #lasers_group.update()
+    
+    #Draw
+    screen.fill(GREY)
+    spaceship_group.draw(screen)
+    #lasers_group.draw(screen)
+    spaceship_group.sprite.lasers_group.draw(screen)
 
 
     pygame.display.update()
